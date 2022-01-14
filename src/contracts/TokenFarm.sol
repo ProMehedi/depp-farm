9f4a1dd01213cf0d6dabb9f01752def98743895b
promehedi <mmhasan563@gmail.com>
2022-01-14 20:27:59 +0600

    Deploy Dai Token

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import './DappToken.sol';
+import './DaiToken.sol';
+
 contract TokenFarm {
   // Variables
-  string public name;
+  string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
 
   // Constructor
-  constructor() {
-    name = "Dapp Token Farm";
+  constructor(DappToken _dappToken, DaiToken _daiToken) {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
   }
 }
